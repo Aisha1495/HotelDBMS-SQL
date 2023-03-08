10cfb4e9954e5c96384957ae66c992f7949e4038
Aisha1495 <aishaalmamari6@gmail.com>
2023-03-07 23:24:05 -0800

    finished task

--- src/HotelSys/Hotels.java
+++ src/HotelSys/Hotels.java
@@ -113,106 +113,110 @@ DriverManager.registerDriver(driver);
         System.out.println("Enter is Active");
         boolean hotel_is_Active = sc.nextBoolean();
         
-        System.out.println("___second Table___");
-        System.out.println("Enter room type id: ");
-        int RoomT_id = sc.nextInt();
-        System.out.println("Enter Room Type: ");
-        String room_type_name = sc.next();
-        System.out.println("Enter creaed data: ");
-        String created_data = sc.next();
-        System.out.println("Enter update data: ");
-        String updated_date = sc.next();
-        System.out.println("Enter room type is Active True\\False");
-        boolean is_Active = sc.nextBoolean();
+//        System.out.println("___second Table___");
+//        System.out.println("Enter room type id: ");
+//        int RoomT_id = sc.nextInt();
+//        System.out.println("Enter Room Type: ");
+//        String room_type_name = sc.next();
+//        System.out.println("Enter creaed data: ");
+//        String created_data = sc.next();
+//        System.out.println("Enter update data: ");
+//        String updated_date = sc.next();
+//        System.out.println("Enter room type is Active True\\False");
+//        boolean is_Active = sc.nextBoolean();
+//        
+//        System.out.println("___third Table___");
+//        System.out.println("Enter Room id: ");
+//        int Room_id = sc.nextInt();
+//        System.out.println("Enter room type id ");
+//        int room_type_id= sc.nextInt();
+//        System.out.println("Enter hotel id");
+//        int hotel_id = sc.nextInt();
+//        System.out.println("Enter room created date ");
+//        String Room_created_date=sc.next();
+//        System.out.println("Enter room updated date ");
+//        String Room_updated_date=sc.next();
+//        System.out.println("Enter room is Active True\\False");
+//        boolean room_is_Active = sc.nextBoolean();
+//        
+//        System.out.println("___4th Table___");
+//        System.out.println("Enter guest id");
+//        int Guest_id = sc.nextInt();
+//        System.out.println("Enter guset name");
+//        String guest_name=sc.next();
+//        System.out.println("Enter guset phone");
+//        String guest_phone=sc.next();
+//        System.out.println("Enter guest accompanying members");
+//        int guest_accompanying_members=sc.nextInt();
+//        System.out.println("Enter guest payment amount");
+//        int guest_payment_amount = sc.nextInt();
+//        System.out.println("Enter guset created date");
+//        String guest_created_date=sc.next();
+//        System.out.println("Enter guest updated date");
+//        String guest_updated_date=sc.next();
+//        System.out.println("Enter guest hotel is_Active True\\False");
+//        boolean guest_hotel_is_Active=sc.nextBoolean();
+//        
+//        System.out.println("___5th Table___");
+//        System.out.println("Enter Employee type id");
+//        int EmployeeT_id=sc.nextInt();
+//        System.out.println("Enter employee name");
+//        String employee_type_name=sc.next();
+//        System.out.println("Enter employee created date");
+//        String empT_created_date=sc.next();
+//        System.out.println("Enter employee update date");
+//        String empT_updated_date=sc.next();
+//        System.out.println("Enter employee is active True\\False");
+//        boolean empT_is_Active=sc.nextBoolean();
+//        
+//        System.out.println("___6th Table___");
+//        System.out.println("Enter Employee id");
+//        int Employee_id=sc.nextInt();
+//        System.out.println("Enter Employee name");
+//        String employee_name=sc.next();
+//        System.out.println("Enter employee name");
+//        String employee_phone=sc.next();
+//        System.out.println("Enter employee type id");
+//        int employee_type_id=sc.nextInt();
+//        System.out.println("Enter room id ");
+//        int room_id=sc.nextInt();
+//        System.out.println("Enter employee created date");
+//        String Employee_created_date=sc.next();
+//        System.out.println("Enter employee update date");
+//        String Employee_updated_date=sc.next();
+//        System.out.println("Enter employee is active True\\False");
+//        boolean Employee_is_Active=sc.nextBoolean();
+//        
         
-        System.out.println("___third Table___");
-        System.out.println("Enter Room id: ");
-        int Room_id = sc.nextInt();
-        System.out.println("Enter room type id ");
-        int room_type_id= sc.nextInt();
-        System.out.println("Enter hotel id");
-        int hotel_id = sc.nextInt();
-        System.out.println("Enter room created date ");
-        String Room_created_date=sc.next();
-        System.out.println("Enter room updated date ");
-        String Room_updated_date=sc.next();
-        System.out.println("Enter room is Active True\\False");
-        boolean room_is_Active = sc.nextBoolean();
         
-        System.out.println("___4th Table___");
-        System.out.println("Enter guest id");
-        int Guest_id = sc.nextInt();
-        System.out.println("Enter guset name");
-        String guest_name=sc.next();
-        System.out.println("Enter guset phone");
-        String guest_phone=sc.next();
-        System.out.println("Enter guest accompanying members");
-        int guest_accompanying_members=sc.nextInt();
-        System.out.println("Enter guest payment amount");
-        int guest_payment_amount = sc.nextInt();
-        System.out.println("Enter guset created date");
-        String guest_created_date=sc.next();
-        System.out.println("Enter guest updated date");
-        String guest_updated_date=sc.next();
-        System.out.println("Enter guest hotel is_Active True\\False");
-        boolean guest_hotel_is_Active=sc.nextBoolean();
+
         
-        System.out.println("___5th Table___");
-        System.out.println("Enter Employee type id");
-        int EmployeeT_id=sc.nextInt();
-        System.out.println("Enter employee name");
-        String employee_type_name=sc.next();
-        System.out.println("Enter employee created date");
-        String empT_created_date=sc.next();
-        System.out.println("Enter employee update date");
-        String empT_updated_date=sc.next();
-        System.out.println("Enter employee is active True\\False");
-        boolean empT_is_Active=sc.nextBoolean();
-        
-        System.out.println("___6th Table___");
-        System.out.println("Enter Employee id");
-        int Employee_id=sc.nextInt();
-        System.out.println("Enter Employee name");
-        String employee_name=sc.next();
-        System.out.println("Enter employee name");
-        String employee_phone=sc.next();
-        System.out.println("Enter employee type id");
-        int employee_type_id=sc.nextInt();
-        System.out.println("Enter room id ");
-        int room_id=sc.nextInt();
-        System.out.println("Enter employee created date");
-        String Employee_created_date=sc.next();
-        System.out.println("Enter employee update date");
-        String Employee_updated_date=sc.next();
-        System.out.println("Enter employee is active True\\False");
-        boolean Employee_is_Active=sc.nextBoolean();
-        
-        
-        
-        String sql1 = "insert into Table_1 valuse ("+id+","+hotel_name+"',"+hotel_location+"',"+hotel_created_data+"',"
-        +hotel_updated_date+"',"+hotel_is_Active+"')";
+       
         
         for  (int i=0; i<10001; i++) {
         	System.out.println(hotel_name+i);
+            String sql1 = "insert into Hotels values("+id+i+",'"+hotel_name+i+"','"+hotel_location+"','"+hotel_created_data+"','"
+                    +hotel_updated_date+"','"+hotel_is_Active+"')";
+            st.execute(sql1);
         } 
         
         
+
         
         
-        
-        Integer m = st.executeUpdate(sql);
-        if (m >= 1) {
-System.out.println("inserted successfully : " + sql);
-} else {
-System.out.println("insertion failed");
-}
-        String sql2 = "Select * from Table_1";
-ResultSet resultSet = st.executeQuery(sql2);
-         while (resultSet.next()) {
+       // Integer m = st.executeUpdate(sql);
+//        if (m >= 1) {
+//System.out.println("inserted successfully : " + sql);
+//} else {
+//System.out.println("insertion failed");
+//}
+//        String sql2 = "Select * from Table_1";
+//ResultSet resultSet = st.executeQuery(sql2);
+//         while (resultSet.next()) {
 //System.out.println(resultSet.getString("name"));
 //System.out.println(resultSet.getString("roll"));
 //System.out.println(resultSet.getString("cls"));
-}
+//}
       
         
         con.close();
